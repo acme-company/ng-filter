67c0c4d159858797955762f079872da0558c7de8
pixelbits <pixelbits100@gmail.com>
2017-05-29 23:47:56 -0600

    fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { FilterPipe } from './filter.pipe';
+import { FilterService } from "./filter.service";
 
 @NgModule({
   imports: [],
   declarations: [ FilterPipe ],
+  providers: [FilterService],
   exports: [ FilterPipe ]
 })
 export class AppModule {
